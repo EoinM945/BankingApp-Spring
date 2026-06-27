cfbc1f30ec1d6efe61d4432e9ea198a311308617
EoinM945 <smiley10494@gmail.com>
2026-06-27 15:12:16 +0100

    set up application and database conection

---
+++ src/main/java/com/masterbank/masterbank/MasterbankApplication.java
@@ -0,0 +1,13 @@
+package com.masterbank.masterbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masterbank/masterbank/MasterbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterbank.masterbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
